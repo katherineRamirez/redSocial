cffed396650b4d3953fbca9eaf9fe61664429d64
katherineRamirez <oeillet8_5@hotmail.com>
2018-01-03 23:19:49 -0300

    validar usuario

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,30 @@
-(function() {
+/* Esta variable contiene los criterios que debe seguir para poder 
+   validar el Email */
+	var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+$(document).ready(function(){
+/* Función con evento click para el botón ingresar */
+  $('#btnLogin').click(function(){ 
+/* Variables que contienen los valores de email y password en la
+	sección de ingreso */
+	var email = $('#txtEmail').val();
+    var password =  $('#txtPassword').val();
 
-//Inicializar Firebase
-  const config = {
-    apiKey: "AIzaSyAQdZCxZy2dp0wTFUnCAnARwV4J0dYiJ78",
-    authDomain: "redsocial-4286a.firebaseapp.com",
-    databaseURL: "https://redsocial-4286a.firebaseio.com",
-    projectId: "redsocial-4286a",
-    storageBucket: "redsocial-4286a.appspot.com",
-    messagingSenderId: "652533453318"
-  };
-  firebase.initializeApp(config);
+/* Condicional para comparar el email ingresado con los criterios 
+	válidos designados en la variable expr */
+	if(email == "" || !expr.test(email)){
+	   $('#message1').fadeIn();
+	   return false;
+	}else{
+	   $('#message1').fadeOut();
+/* Condicional para hacer aparecer el mensaje en el caso que no
+	se ingrese nada en el input del password */
+	if(password == ""){
+	   $('#message2').fadeIn();
+	   return false;
+	}else{
+	   $('#message2').fadeOut();
+	}
+	}
+  })
 
-  // Obtener elementos
-  const txtEmail = document.getElementById('txtEmail');
-  const txtPassword = document.getElementById('txtPassword');
-  const btnLogin = document.getElementById('btnLogin');
-  const btnSignUp = document.getElementById('btnSignUp');
-
-  // Añadir Evento login
-  btnLogin.addEventListener('click', e => {
-    //Obtener email y pass
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-    // Sign in
-    const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));   
-  });
-
-  // Añadir evento signup
-  btnSignUp.addEventListener('click', e => {
-    // Obtener email y pass
-    // TODO: comprobar que el email sea real
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-    // Sign in
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-  });
-
-  
-  });
+});
